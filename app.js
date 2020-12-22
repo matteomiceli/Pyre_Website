8ce152c6cf5eddfc4828a48aadcac7ee6e1e7f44
matteomiceli <omatteomiceli@gmail.com>
2020-12-21 19:12:37 -0800

    removed popups, now content displays as sidebar

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
+// MAP AREAS
 const faneArea = document.querySelector('#fane');
-const popup = document.querySelector('.popup-overlay');
-const closeBtn = document.querySelector('#close-btn');
 
-closeBtn.addEventListener('click', () => {
-    popup.classList.remove('overlay-active');
-});
+// PAGE ELEMENTS
+const popup = document.querySelector('.popup-overlay');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar-content');
+
+
+
+
 
 faneArea.addEventListener('click', () => {
-    popupActive();
+    clear();
+    // createClose(sidebar);
+
+    const faneHeader = document.createElement('h3');
+    faneHeader.innerHTML = 'Fane'
+    sidebar.appendChild(faneHeader);
     
+    const faneContent = document.createElement('p');
+    faneContent.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua. At lectus urna duis 
+    convallis convallis tellus id.`;
+    sidebar.appendChild(faneContent);
 });
 
 
 
+
+
+
+
+
+closeBtn.addEventListener('click', () => {
+    clear();
+    const placeholder = document.createElement('p');
+    placeholder.innerHTML = '<em>Click a place on the map to begin exploring Pyre!</em>'
+    placeholder.classList.add('placeholder');
+    sidebar.appendChild(placeholder);
+});
+
+// Generates close button
+const createClose = (parentElement) => {
+    const closeX = document.createElement('h4');
+    closeX.innerHTML = '✕';
+    closeX.classList.add('close-btn');
+    parentElement.appendChild(closeX);
+    console.log('hmm')
+}
+
+const clear = () => {
+    sidebar.innerHTML = '';
+}
 
 const popupActive = () => {
     popup.classList.add('overlay-active');
-};
+}
