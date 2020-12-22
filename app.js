8a00c1229492306384bc653d5e329e2584138e0a
matteomiceli <omatteomiceli@gmail.com>
2020-12-21 16:35:57 -0800

    more styling of text popup

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const closeBtn = document.querySelector('#close-btn');
 
 closeBtn.addEventListener('click', () => {
     popup.classList.remove('overlay-active');
-})
+});
 
 faneArea.addEventListener('click', () => {
     popupActive();
     
-})
+});
 
 
 
 
 const popupActive = () => {
     popup.classList.add('overlay-active');
-}
+};
